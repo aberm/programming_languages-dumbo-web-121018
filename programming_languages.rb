e56c55bc1d19a3afd8e62d9036ebe5b058fcae1c
Menachem Aber <abermendel@gmail.com>
2018-11-28 21:47:29 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,14 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  new_langs = {}
+  
+  languages.each do |styles, langs|
+    langs.each do |types, values|
+      # binding.pry
+      new_langs[types] = {type: values.values, style: styles}
+    end
+  end
+  new_langs
+  
 end
